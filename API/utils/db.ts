f7aaa7cf5802e6890c5d0b42084488b6cac4b82d
Alexandre78R <alexandre.renard98@gmail.com>
2025-05-19 12:45:50 +0200

    add script migrate bdd and default bdd json backend

--- API/utils/db.ts
+++ API/utils/db.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-const dbPath = path.join(__dirname, "../db.json");
+const dbPath = path.join(__dirname, "../data/db.json");
 
 export interface User {
   id: number;
