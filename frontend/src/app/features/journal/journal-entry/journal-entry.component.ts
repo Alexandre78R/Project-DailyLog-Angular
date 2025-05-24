f4ad6eaf3ce9195f60d1bdb0eaf78eaef5712092
Alexandre78R <alexandre.renard98@gmail.com>
2025-05-24 17:05:39 +0200

    add structure journal entry

--- frontend/src/app/features/journal/journal-entry/journal-entry.component.ts
+++ frontend/src/app/features/journal/journal-entry/journal-entry.component.ts
@@ -3,18 +3,37 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../auth/services/auth.service';
 import { LogoutButtonComponent } from '../../auth/logout-button/logout-button.component';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-journal-entry',
-  imports: [LogoutButtonComponent, CommonModule],
+  standalone: true,
+  imports: [LogoutButtonComponent, CommonModule, ReactiveFormsModule],
   templateUrl: './journal-entry.component.html',
   styleUrls: ['./journal-entry.component.css']
 })
 export class JournalEntryComponent {
+  isLoggedIn$!: ReturnType<AuthService['isLoggedIn$']>;
+  journalForm: FormGroup;
 
-  isLoggedIn$!: ReturnType<AuthService['isLoggedIn$']>;  // propriété non initialisée ici
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.isLoggedIn$ = this.auth.isLoggedIn$();
 
-  constructor(private auth: AuthService, private router: Router) {
-    this.isLoggedIn$ = this.auth.isLoggedIn$(); // initialisation dans le constructeur, après injection
+    this.journalForm = this.fb.group({
+      title: ['', Validators.required],
+      content: ['', Validators.required],
+      mood: ['neutral', Validators.required],
+      date: [new Date().toISOString()]
+    });
+  }
+
+  onSubmit() {
+    if (this.journalForm.valid) {
+      console.log('Form submitted', this.journalForm.value);
+    }
   }
 }
