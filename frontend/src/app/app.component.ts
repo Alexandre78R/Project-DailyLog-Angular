0f22479466dad2a02b0d3447fcae1983443b677f
Alexandre78R <alexandre.renard98@gmail.com>
2025-05-25 01:39:12 +0200

    add mood-indicator and test composent for app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { NoteCardComponent } from './shared/components/note-card/note-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, CommonModule],
+  imports: [RouterOutlet, NavbarComponent, CommonModule, NoteCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/shared/components/mood-indicator/mood-indicator.component.ts
+++ frontend/src/app/shared/components/mood-indicator/mood-indicator.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-mood-indicator',
-  imports: [],
+  standalone: true,
   templateUrl: './mood-indicator.component.html',
-  styleUrl: './mood-indicator.component.css'
 })
 export class MoodIndicatorComponent {
+  @Input() mood: 'happy' | 'neutral' | 'sad' = 'neutral';
 
+  get emoji(): string {
+    switch (this.mood) {
+      case 'happy':
+        return '😀';
+      case 'sad':
+        return '😔';
+      default:
+        return '😐';
+    }
+  }
+
+  get color(): string {
+    switch (this.mood) {
+      case 'happy':
+        return 'text-yellow-400';
+      case 'sad':
+        return 'text-blue-400';
+      default:
+        return 'text-gray-400';
+    }
+  }
 }
