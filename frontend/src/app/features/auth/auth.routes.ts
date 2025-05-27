8e85d44ce9aa5137ac50ffac7491b1c7fd1a6e35
Alexandre78R <alexandre.renard98@gmail.com>
2025-05-27 13:00:52 +0200

    fixed auth verify no-auth-gard

--- frontend/src/app/features/auth/auth.routes.ts
+++ frontend/src/app/features/auth/auth.routes.ts
@@ -1,20 +1,15 @@
 import { Routes } from '@angular/router';
+import { NoAuthGuard } from './guards/auth.no-auth.guard';
 
 export const authRoutes: Routes = [
   {
     path: 'login',
-    loadComponent: () =>
-      import('./login/login.component').then(m => m.LoginComponent),
-    canActivate: [() =>
-      import('./guards/auth.no-auth.guard').then(m => m.NoAuthGuard)
-    ],
+    loadComponent: () => import('./login/login.component').then(m => m.LoginComponent),
+    canActivate: [NoAuthGuard],
   },
   {
     path: 'register',
-    loadComponent: () =>
-      import('./register/register.component').then(m => m.RegisterComponent),
-    canActivate: [() =>
-      import('./guards/auth.no-auth.guard').then(m => m.NoAuthGuard)
-    ],
+    loadComponent: () => import('./register/register.component').then(m => m.RegisterComponent),
+    canActivate: [NoAuthGuard],
   },
 ];
