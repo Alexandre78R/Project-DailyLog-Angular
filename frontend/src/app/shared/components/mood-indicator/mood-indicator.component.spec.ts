d72e934afbb56e80cd359f5004e98abab8c32e30
Alexandre78R <alexandre.renard98@gmail.com>
2025-06-06 00:43:08 +0200

    add test mood indicator

--- frontend/src/app/shared/components/mood-indicator/mood-indicator.component.spec.ts
+++ frontend/src/app/shared/components/mood-indicator/mood-indicator.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MoodIndicatorComponent } from './mood-indicator.component';
+import { By } from '@angular/platform-browser';
 
 describe('MoodIndicatorComponent', () => {
   let component: MoodIndicatorComponent;
@@ -10,15 +10,52 @@ describe('MoodIndicatorComponent', () => {
     await TestBed.configureTestingModule({
       imports: [MoodIndicatorComponent],
     }).compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(MoodIndicatorComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return correct emoji for "happy" 😊 mood', () => {
+    component.mood = 'happy';
+    expect(component.emoji).toBe('😊');
+  });
+
+  it('should return correct emoji for "sad" 😢 mood', () => {
+    component.mood = 'sad';
+    expect(component.emoji).toBe('😢');
+  });
+
+  it('should return correct emoji for "neutral" 😐 mood', () => {
+    component.mood = 'neutral';
+    expect(component.emoji).toBe('😐');
+  });
+
+  it('should return "❓" emoji for unknown mood', () => {
+    component.mood = 'angry'; // mood non géré
+    expect(component.emoji).toBe('❓');
+  });
+
+  it('should return correct color class for "sad" mood', () => {
+    component.mood = 'sad';
+    expect(component.color).toBe('text-blue-500');
+  });
+
+  it('should return correct color class for "neutral" mood', () => {
+    component.mood = 'neutral';
+    expect(component.color).toBe('text-gray-500');
+  });
+
+  it('should return default color class for "happy" mood', () => {
+    component.mood = 'happy';
+    expect(component.color).toBe('text-yellow-500');
+  });
+
+  it('should return default color class for unknown mood', () => {
+    component.mood = 'excited';
+    expect(component.color).toBe('text-yellow-500');
   });
 });
